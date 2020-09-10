5f67d14b7f993bd8a68f791a145b3804883b617e
minyongsim <alsdyd5411@naver.com>
2020-09-10 17:59:30 +0900

    ZZZZZ

--- js/common.js
+++ js/common.js
@@ -1,15 +1,6 @@
 (function(){
 
-    $('.depth1 > li').hover(
-        function(){
-            $(this).find('.depth2')
-            .stop().slideDown(700)
-        },
-        function(){
-            $(this).find('.depth2')
-            .stop().slideUp(500)
-        }
-    )
+  
 
     $('#simBox').load('main.html')
     $('.login_box > a').on('click',function(e){
